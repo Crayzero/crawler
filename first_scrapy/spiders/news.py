6c451f39a4401242bb59a17003495088f33c29fc
chen <chen154556138@163.com>
2014-05-15 18:30:12 +0800

    startup news spider

--- first_scrapy/spiders/news.py
+++ first_scrapy/spiders/news.py
@@ -15,10 +15,11 @@ class NewsSpider(Spider):
 	start_urls=['http://news.dbanotes.net',]
 	base=start_urls[0]
 	def parse(self,response):
+		self.log('%s be crawled.'%response.url)
 		sel=Selector(response)
 		newss=sel.xpath('//td[@class="title"]/a')
 		log_file=open('a.txt','a+')
-		items=[]
+		requests=[]
 		for news in newss:
 			title=news.xpath('text()').extract()
 			link=news.xpath('@href').extract()
@@ -27,9 +28,10 @@ class NewsSpider(Spider):
 				item['title']=title[0]
 				item['link']=link[0]
 				a=u'%s,%s\n'%(title[0],link[0])
-				print a
+				#print a
 				log_file.write(a.encode('utf-8'))
+				yield item
 				if title[0]=='More':
 					url=self.base+link[0]
-					items.append(self.make_requests_from_url(url))
-		return items
+					yield self.make_requests_from_url(url)
+#或者可以返回一个scrapy.http.Request的对象，使用方法为：Request(url,callback='xxx')
